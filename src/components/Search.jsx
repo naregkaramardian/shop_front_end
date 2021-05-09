26e55671a9b562fc1727c54b38a7ab4eb92e5db8
Nareg Karamardian <nareg.Karamardian@gmail.com>
2021-05-09 17:19:42 -0400

    addedsearch year and fixed design

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,6 +6,7 @@ import SearchItem from './search_item'
 export default function Search({ addNominationList, nominations }) {
   // State and setter for search term
   const [searchTerm, setSearchTerm] = useState('')
+  const [year, setYear] = useState('')
   // State and setter for search results
   const [results, setResults] = useState([])
   // State for search status (whether there is a pending API request)
@@ -33,7 +34,7 @@ export default function Search({ addNominationList, nominations }) {
   // API search function
   const searchCharacters = async (search) => {
     const apiKey = 'aa21a4b1'
-    const queryString = `apikey=${apiKey}&s=${search}`
+    const queryString = `apikey=${apiKey}&s=${search}&y=${year}`
     const url = `https://www.omdbapi.com/?${queryString}`
     const response = await fetch(url)
     const data = await response.json()
@@ -44,8 +45,9 @@ export default function Search({ addNominationList, nominations }) {
   return (
     <div>
       <div className='my-6 mx-2 md:m-6 bg-white overflow-hidden shadow rounded-lg'>
-        <div className='p-0.5 w-full h-16'>
-          <input className='w-full h-full shadow rounded-lg border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent' placeholder='Search Movies' onChange={(e) => setSearchTerm(e.target.value)} />
+        <div className='flex p-0.5 w-full h-16'>
+          <input className='m-1  w-2/3 h-full shadow rounded-lg px-5 border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent' placeholder='Search Movies' onChange={(e) => setSearchTerm(e.target.value)} />
+          <input className='m-1  w-1/3 h-full shadow rounded-lg px-5 border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent' placeholder='Year' onChange={(e) => setYear(e.target.value)} />
         </div>
         <div className='bg-gray-50 px-4 py-5 sm:p-6'>
           {isSearching && <div>Searching ...</div>}
@@ -53,7 +55,7 @@ export default function Search({ addNominationList, nominations }) {
           {results.Search &&
             results.Search.map((result) => (
               <div key={result.imdbID + uuidv4()} className='w-full '>
-                <SearchItem id={result.imdbID} title={result.Title} poster={result.Poster} addNominationList={addNominationList} result={result} nominations={nominations} />
+                <SearchItem addNominationList={addNominationList} result={result} nominations={nominations} />
               </div>
             ))}
         </div>
--- src/components/search_item.jsx
+++ src/components/search_item.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-
-const SearchItem = ({ id, title, poster, result, addNominationList, nominations }) => {
+import placeholder from '../images/placeholder-image.jpg'
+const SearchItem = ({ result, addNominationList, nominations }) => {
   const [isNominated, setIsNominated] = useState(false)
 
   const checkNominations = (nominations, id) => {
@@ -12,22 +12,23 @@ const SearchItem = ({ id, title, poster, result, addNominationList, nominations 
   }
 
   useEffect(() => {
-    checkNominations(nominations, id)
-  }, [nominations, id])
+    checkNominations(nominations, result.imdbID)
+  }, [nominations, result.imdbID])
   return (
-    <div className='flex bg-white dark:bg-gray-800 rounded-lg  md:h-64 my-4 md:m-7 w-full'>
+    <div className='flex bg-white dark:bg-gray-800 rounded-lg  md:h-64 my-4 md:m-7 '>
       <div className='flex-none w-24 md:w-48  relative'>
-        <img src={poster} alt={title} className='absolute rounded-lg inset-0 w-full max-h-48 m-auto md:min-h-full h-full object-cover' />
+        <img src={placeholder} alt={result.Title} className='absolute rounded-lg inset-0 w-full max-h-48 m-auto md:min-h-full h-full object-cover' />
+        
       </div>
       <form className='flex-auto p-6 h-64'>
         <div className='flex flex-wrap'>
-          <h1 className='flex-auto text-xl font-semibold dark:text-gray-50'>{title}</h1>
+          <h1 className='flex-auto text-xl font-semibold dark:text-gray-50'>{result.Title}</h1>
           <div className='text-xl font-semibold text-gray-500 dark:text-gray-300'>$110.00</div>
-          <div className='w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300 mt-2'>In stock</div>
+          <div className='w-full flex-none text-sm font-medium text-gray-500 dark:text-gray-300 mt-2'>{result.Year}</div>
         </div>
-
+        
         <div className='flex mb-4 text-sm font-medium'>
-          <button onClick={() => addNominationList(result)} disabled={nominations.length > 4 || isNominated} className='disabled:opacity-60 disabled:cursor-not-allowed py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full md:w-1/2 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg'>
+          <button onClick={() => addNominationList(result)} disabled={nominations.length > 4 || isNominated} className='disabled:opacity-60 disabled:cursor-not-allowed py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full sm:w-40 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg'>
             Buy now
           </button>
         </div>
